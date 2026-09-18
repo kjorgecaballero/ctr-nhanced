461c14cd87133f8485def004792d5e72420752e5
kjorgecaballero <kja.cruzcaballero@gmail.com>
2026-09-18 09:25:57 -0600

    chore(addon): add kart package __init__.py to repository

--- tools/custom_racers/blender_addon/ctr_racer/kart/__init__.py
+++ tools/custom_racers/blender_addon/ctr_racer/kart/__init__.py
@@ -5,18 +5,20 @@
 
 Fase 1: import + color pickers.
 Fase 2: bake + cut + quantize + save to preset folder.
-Fase 3 (planned): preset browser.
+Fase 3: preset browser + apply to racer.
 """
-from . import state, importer, baker
+from . import state, importer, baker, presets
 
 
 def register():
     state.register()
     importer.register()
     baker.register()
+    presets.register()
 
 
 def unregister():
+    presets.unregister()
     baker.unregister()
     importer.unregister()
     state.unregister()
